7a0db251f4dfcc1a8eef4696b54e5bc5f3f84d4e
CeceMind <cecilia.minder@gmail.com>
2021-04-27 10:59:35 +0200

    Created a JUnit test project

---
+++ TestFacadeEJB_JUnitEE/src/ics/junit/ejb/FacadeBeanTest.java
@@ -0,0 +1,37 @@
+package ics.junit.ejb;
+
+import javax.naming.InitialContext;
+import javax.naming.Context;
+
+import org.ics.facade.FacadeLocal;
+
+
+
+import junit.framework.TestCase;
+
+public class FacadeBeanTest extends TestCase {
+	
+	FacadeLocal facade;
+
+	public FacadeBeanTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		Context context = new InitialContext();
+		
+		facade=(FacadeLocal)context.lookup("java:app/IsEJBProject/Facade!org.ics.facade.FacadeLocal");
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		facade = null;
+	}
+	public void testFacadeMethods() throws Exception { //Är inte tanken att vi ska skapa nya objekt som ska testas i denna metod? 
+		
+	}
+	
+	//WHAT METHODS TO PUT HERE? ALL METHODS IN FacadeLocal's...?
+
+}
