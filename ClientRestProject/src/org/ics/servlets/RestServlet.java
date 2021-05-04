5d94f95484449522af1143c37fd9087acd187a51
ludvigkarsten <ludvigkarsten@gmail.com>
2021-05-04 16:28:08 +0200

    Format on REST files

--- ClientRestProject/src/org/ics/servlets/RestServlet.java
+++ ClientRestProject/src/org/ics/servlets/RestServlet.java
@@ -25,79 +25,80 @@ import org.ics.facade.FacadeLocal;
 public class RestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@EJB
-	FacadeLocal facade;   
+	FacadeLocal facade;
 
-    public RestServlet() {
-        super();
-    }
+	public RestServlet() {
+		super();
+	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String pathInfo = request.getPathInfo();
-		if(pathInfo == null || pathInfo.equals("/")){
-		response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-		 return;
+		if (pathInfo == null || pathInfo.equals("/")) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
 		}
 		String[] splits = pathInfo.split("/");
-		if(splits.length != 2) {
-		 response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-		 return;
+		if (splits.length != 2) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
 		}
 		String id = splits[1];
 		Tournament tour = facade.findTournament(id);
 		sendAsJson(response, tour);
 	}
 
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String pathInfo = request.getPathInfo();
-		if(pathInfo == null || pathInfo.equals("/")){
-		response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-		return;
+		if (pathInfo == null || pathInfo.equals("/")) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
 		}
 		String[] splits = pathInfo.split("/");
-		if(splits.length != 2) {
-		response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-		return;
+		if (splits.length != 2) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+			return;
 		}
 		BufferedReader reader = request.getReader();
 		Tournament tour = parseJsonTournament(reader);
 		System.out.println(tour.getTournamentID());
 		try {
-			
-			 facade.updateTournament(tour);
-			}catch(Exception e) {
-			 System.out.println("facade Update Error");
-			}
-			sendAsJson(response, tour);
+
+			facade.updateTournament(tour);
+		} catch (Exception e) {
+			System.out.println("facade Update Error");
+		}
+		sendAsJson(response, tour);
 	}
 
-	private void sendAsJson(HttpServletResponse response, Tournament tour)
-			 throws IOException {
+	private void sendAsJson(HttpServletResponse response, Tournament tour) throws IOException {
 
-			PrintWriter out = response.getWriter();
-			response.setContentType("application/json");
-			if (tour != null) {
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		if (tour != null) {
 			out.print("{\"txtID\":");
 			out.print("\"" + tour.getTournamentID() + "\"");
 			out.print(",\"txtName\":");
-			out.print("\"" +tour.getTournamentName()+"\"");
+			out.print("\"" + tour.getTournamentName() + "\"");
 			out.print(",\"txtSport\":");
-			out.print("\"" +tour.getSport()+"\"}");		
-			} else {
+			out.print("\"" + tour.getSport() + "\"}");
+		} else {
 			out.print("{ }");
-			}
-			out.flush();
-			}
-	private Tournament parseJsonTournament(BufferedReader br) {
-		 JsonReader jsonReader = null;
-		 JsonObject jsonRoot = null;
-		 jsonReader = Json.createReader(br);
-		 jsonRoot = jsonReader.readObject();
-		 Tournament tour = facade.findTournament(jsonRoot.getString("id"));
-		 tour.setTournamentName(jsonRoot.getString("name"));
-		 tour.setSport(jsonRoot.getString("sport"));
-		 
-		 return tour;
 		}
-}
+		out.flush();
+	}
 
+	private Tournament parseJsonTournament(BufferedReader br) {
+		JsonReader jsonReader = null;
+		JsonObject jsonRoot = null;
+		jsonReader = Json.createReader(br);
+		jsonRoot = jsonReader.readObject();
+		Tournament tour = facade.findTournament(jsonRoot.getString("id"));
+		tour.setTournamentName(jsonRoot.getString("name"));
+		tour.setSport(jsonRoot.getString("sport"));
+
+		return tour;
+	}
+}
